e6bd0d5ce0625e667cb5d796d599bee2a1030ec8
wenyonghua <xiao_xiao8230@126.com>
2021-10-06 22:18:07 +0800

    提交代码全部

--- studyAll/waitingLuck/src/test/java/com/waitig/TestTT.java
+++ studyAll/waitingLuck/src/test/java/com/waitig/TestTT.java
@@ -1,10 +1,13 @@
 package com.waitig;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestTT {
     @Autowired
@@ -12,8 +15,6 @@ public class TestTT {
 
     @Test
     public void ttt(){
-       // redisTemplate.
-              //  redisTemplate.opsForValue();
         redisTemplate.opsForValue().set("name","lzj");
         System.out.println("dsfsf");
     }
