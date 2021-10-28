3889d4257e053336940f9da85c39ba1c191c4bae
wenyonghua <xiao_xiao8230@126.com>
2021-10-28 20:29:31 +0800

    提交项目1

--- studyAll/runscore-merchant/src/main/java/me/zohar/runscore/dictconfig/controller/DictConfigController.java
+++ studyAll/runscore-merchant/src/main/java/me/zohar/runscore/dictconfig/controller/DictConfigController.java
@@ -1,5 +1,7 @@
 package me.zohar.runscore.dictconfig.controller;
 
+import me.zohar.runscore.common.vo.Result;
+import me.zohar.runscore.dictconfig.DictHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/dictconfig")
 public class DictConfigController {
 
-
+    @GetMapping("/findDictItemInCache")
+    @ResponseBody
+    public Result findDictItemInCache(String dictTypeCode) {
+        return Result.success().setData(DictHolder.findDictItem(dictTypeCode));
+    }
 
 }
